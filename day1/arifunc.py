acf30009da419cde8640a7db1bd7ed0967ea9d5f
BharathReddyRamasani <ramasanibharathreddy2004@gmail.com>
2025-09-03 14:06:34 +0530

    commited on some basics of python

---
+++ day1/arifunc.py
@@ -0,0 +1,16 @@
+a,b=map(int,input("enter").split())
+def add(a,b):
+    return a+b
+print(add(a,b))
+def sub(a,b):
+    return a-b
+print(sub(a,b))
+def mul(a,b):
+    return a*b
+print(mul(a,b))
+def div(a,b):
+    return a/b
+print(div(a,b))
+def mod(a,b):
+    return a%b
+print(mod(a,b))
